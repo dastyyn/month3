224ec035b8359e5923fc620295eb73028fc7bc7b
Дастан <146834313+dastyyn@users.noreply.github.com>
2023-11-21 06:38:17 +0600

    незначительные изменения

--- handlers/my_info.py
+++ handlers/my_info.py
@@ -5,7 +5,7 @@ from aiogram.filters import Command
 my_info_router = Router()
 
 
-@my_info_router.message(Command("myinfo"))
+@my_info_router.message(Command("my_info"))
 async def my_info(message: types.Message):
     await message.answer(f"Your id: {message.from_user.id}\n"
                          f"your first name is {message.from_user.first_name}\n"
