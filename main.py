b884c14b7dfe9c437b81f8a131ab658154abbfe9
Дастан <146834313+dastyyn@users.noreply.github.com>
2023-11-24 22:38:03 +0600

    Добавлено меню и исправлена ошибка

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 import asyncio
-from bot import dp, bot
-from handlers import my_info_router, picture_router, start_router
+
+from aiogram.types import BotCommand
+
+from bot import bot, dp
+from handlers import (my_info_router, picture_router,
+                      start_router)
 
 
 async def main():
+    await bot.set_my_commands([
+        BotCommand(command="start", description="Начало"),
+        BotCommand(command="my_info", description="Информация обо мне"),
+        BotCommand(command="picture", description="Случайная картинка"),
+        BotCommand(command="search", description="Выбор жанра")
+    ])
+
     dp.include_router(start_router)
     dp.include_router(my_info_router)
     dp.include_router(picture_router)
-    await dp.start_polling(bot)
+
+    try:
+        await dp.start_polling(bot)
+    finally:
+        await bot.session.close()
 
 
 if __name__ == "__main__":
